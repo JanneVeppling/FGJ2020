2a7a8462eaa82e80c88d83613709077797836cc0
Tuubbi <45103287+Tuubbi@users.noreply.github.com>
2020-02-01 00:30:33 +0200

    Scriptit
    oikeaan sceneen ja projektiin

---
+++ Assets/Topin Juttui/Country.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Country : MonoBehaviour
+{
+    private Plague plague;
+
+
+
+    public int id;
+    List<int> neighbourIds = new List<int>();
+    float outsideSpreadChance;
+    float insideSpreadChance;
+
+    public float wealth;   // 0 to 2?
+    public float density;  // 0 to 2?
+    public float tourism;  // 0 to 2?
+    public bool EU;
+
+    public int populationTotal;
+    public int numberOfHealthy;
+    public int numberOfInfected;
+    public int numberOfDeah;
+    public int numberOfVaccinated;
+    public int numberOfTransports;
+
+    public int sentVaccinessPerSent;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Awake()
+    {
+        plague = GetComponent<Plague>();
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+    void SpreadInside()
+    {
+        float plagueInsideChance = plague.insideChance;
+
+        if (numberOfHealthy <= 0)
+        {
+            float chanceRoll = Random.Range(0.0f, 100.0f);
+
+            float infectChance = insideSpreadChance * plagueInsideChance * density;
+
+            if (infectChance > chanceRoll)
+            {
+                float infectedRoll = Random.Range(1.0f, 6.0f);
+                float infectedPeopleF = (numberOfHealthy / 100 * infectChance) * infectedRoll;
+                int infectedPeople = (int)infectedPeopleF;
+                infectedPeople = infectedPeople + (populationTotal / 200);
+
+                numberOfHealthy = numberOfHealthy - infectedPeople;
+                numberOfInfected = numberOfInfected + infectedPeople;
+
+            }
+
+        }
+    }
+
+    void SpreadOutside()
+    {
+
+        float plagueOutsideChance = plague.outsideChance;
+
+        if (numberOfHealthy <= 0)
+        {
+
+
+            float chanceRoll = Random.Range(0.0f, 100.0f);
+
+            float infectChance = outsideSpreadChance * plagueOutsideChance * tourism;
+
+            if (infectChance > chanceRoll)
+            {
+
+                float infectedPeople = (numberOfHealthy / 100 * infectChance);
+                infectedPeople = infectedPeople + (populationTotal / 200);
+            }
+        }
+
+    }
+
+
+
+}
---
+++ Assets/Topin Juttui/Plague.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plague : MonoBehaviour
+{
+    public float drugRes;
+    float mutationChance;
+    public float outsideChance;
+    public float insideChance;
+    public bool airborne;
+    float airborneMutationChance;
+
+
+    //   >
+
+    private void Mutate()
+    {
+        float chanceRoll = Random.Range(0.0f, 100.0f);
+
+        if (airborneMutationChance >= chanceRoll & airborne == false)
+        {
+            airborne = true;
+
+        }
+
+        else if (chanceRoll > 90)
+        {
+
+            //do seriois mutation stuff
+        }
+
+
+        else if (chanceRoll > 60)
+        {
+
+            //do smoll mutation stuff
+        }
+    }
+
+
+
+}
