856aeba4a1e8d670fda3c063430d5e5261e6f761
Heinahattu <janne.veppling@hotmail.com>
2020-02-02 02:29:52 +0200

    HudFix

---
+++ Assets/janneScriptit/ColorChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    SpriteRenderer sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // Change the 'color' property of the 'Sprite Renderer'
+            sprite.color = new Color(1, 0, 0, 1);
+        }
+    }
+}
